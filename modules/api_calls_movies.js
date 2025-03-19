8fb1778d2de23bcdd5b5845818f095c58c400acc
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-03-19 11:29:01 +0100

    get top rated and get search movies

--- modules/api_calls_movies.js
+++ modules/api_calls_movies.js
@@ -18,3 +18,45 @@ export async function fetchPopularMovies(options) {
         console.error(error);
     } 
 }
+
+export async function fetchTopRatedMovies(options) {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
+
+        console.log(response);
+
+        if(!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);        
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data.results;
+    } 
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function fetchSearchMovies(options, searchInput) {
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchInput)}&include_adult=true&language=en-US&page=1`, options);
+        
+        console.log(response);
+
+        if(!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);        
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data.results;
+    }
+    catch (error) {
+        console.error(error);
+    } 
+}
