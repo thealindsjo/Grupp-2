9a60b39109b6342645a49f78f1bc8e6c29c04c66
SlyBiggie <146238230+SlyBiggie@users.noreply.github.com>
2025-03-19 15:01:21 +0100

    tv_series

---
+++ modules/api_calls_tv.js
@@ -0,0 +1,41 @@
+export async function fetchTopRatedTvSeries(options) {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/tv/popular?language=en-US&page=1', options);
+
+        console.log(response);
+
+        if(!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);        
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data.results;
+    } 
+    catch (error) {
+        console.error(error);
+    } 
+}
+
+export async function fetchPopularTvSeries(options) {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options);
+
+        console.log(response);
+
+        if(!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);        
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data.results;
+    } 
+    catch (error) {
+        console.error(error);
+    } 
+}
