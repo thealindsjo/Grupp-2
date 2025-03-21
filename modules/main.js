7325a8ad7946a10e7d5ba283882c876e942881f1
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-03-21 14:37:04 +0100

    tv series can display and search

--- modules/main.js
+++ modules/main.js
@@ -56,10 +56,12 @@ if (document.body.id === 'movies') {
 
 if (document.body.id === 'tvSeries') {
     import("./api_calls_tv.js").then(module => {
-        const { fetchPopularTvSeries, fetchTopRatedTvSeries } = module;
+        const { fetchPopularTvSeries, fetchTopRatedTvSeries, fetchSearchTvSeries } = module;
 
+        const contentContainer = document.querySelector('#contentContainer');
         const popularBtn = document.querySelector('#popular-btn');
         const topRatedBtn = document.querySelector('#top-rated-btn');
+        const searchField = document.querySelector('#search-field');
 
         const options = {
             method: 'GET',
@@ -71,13 +73,28 @@ if (document.body.id === 'tvSeries') {
 
         popularBtn.addEventListener('click', async () => {
             const popularSeries = await fetchPopularTvSeries(options);
-            displayTvSeries(popularSeries);
+            displayTvSeries(popularSeries, contentContainer);
         });
 
         topRatedBtn.addEventListener('click', async () => {
             const topRatedSeries = await fetchTopRatedTvSeries(options);
-            displayTvSeries(topRatedSeries);
+            displayTvSeries(topRatedSeries, contentContainer);
         });
+
+      
+        const delay = 300;
+        let timer;
+
+        searchField.addEventListener('input', async (event) => {
+            const searchInput = event.target.value.trim();
+            
+            clearTimeout(timer);
+            timer = setTimeout(async () => {
+                
+            const searchedSeries = await fetchSearchTvSeries(options, searchInput)
+            displayMovies(searchedSeries, contentContainer);
+            }, delay);
+        })
 
     }).catch(error => console.error("Error loading TV module:", error));
 }
